0649973b9afc59f62217ef8fb65519de66cf6247
priyamesh <priyamesh28@gmail.com>
2023-12-23 23:05:35 +0530

    use usercard component

--- src/components/AddUsers.js
+++ src/components/AddUsers.js
@@ -1,5 +1,6 @@
 import React, { useEffect, useState } from 'react'
 import { Link } from 'react-router-dom'
+import UserCard from './UserCard'
 
 const AddUsers = () => {
 
@@ -68,37 +69,11 @@ const AddUsers = () => {
                 </div>
             </div>
 
-            {/* Rendering list */}
+            {/* Rendering users card */}
             <div className="container">
                 <div className="row">
                     {users.map((user, idx) => (
-                        <div className="col-md-4 mb-3" key={idx}>
-                            <Link to={`/user?user_id=${user.id}`} className="text-decoration-none">
-                                <div className="card" style={{ width: '18rem' }}>
-                                    <div className="card-body d-flex align-items-center">
-                                        <img
-                                            src={user.avatar_url}
-                                            className="card-img-top rounded-circle mx-3"
-                                            alt="User Avatar"
-                                            style={{ width: '3rem', height: '3rem', objectFit: 'cover' }}
-                                        />
-                                        <h6 className="card-title text-center">
-                                            <a href={user.html_url} className="card-link">{user.name}</a>
-                                        </h6>
-                                    </div>
-                                    <ul className="list-group list-group-flush">
-                                        <li className="list-group-item">Company: {user.company}</li>
-                                        <li className="list-group-item">Location: {user.location}</li>
-                                        <li className="list-group-item">Email: {user.email}</li>
-                                        <li className="list-group-item">Followers: {user.followers}</li>
-                                        <li className="list-group-item">Following: {user.following}</li>
-                                        <li className="list-group-item">Public Repos: {user.public_repos}</li>
-                                        <li className="list-group-item">Public Gists: {user.public_gists}</li>
-                                        <li className="list-group-item">Member Since: {user.created_at}</li>
-                                    </ul>
-                                </div>
-                            </Link>
-                        </div>
+                        <UserCard user={user} key={idx} />
                     ))}
                 </div >
             </div >
--- src/components/UserCard.js
+++ src/components/UserCard.js
@@ -1,27 +1,35 @@
 import React from "react";
+import { Link } from 'react-router-dom'
 
-const UserCard = (props) => {
-    // console.log(props);
-
+const UserCard = ({ user, idx }) => {
     return (
         <>
-            <div>
-                <div className="card" style={{ width: '18rem' }}>
-                    {/* <img src="..." class="card-img-top" alt="..."> */}
-                    <div className="card-body">
-                        <h6 className="card-title">name</h6>
-                        <a href="{data.html_url}" className="card-link">name</a>
+            <div className="col-md-4 mb-3" key={idx}>
+                <Link to={`/user?user_id=${user.id}`} className="text-decoration-none">
+                    <div className="card" style={{ width: '18rem' }}>
+                        <div className="card-body d-flex align-items-center">
+                            <img
+                                src={user.avatar_url}
+                                className="card-img-top rounded-circle mx-3"
+                                alt="User Avatar"
+                                style={{ width: '3rem', height: '3rem', objectFit: 'cover' }}
+                            />
+                            <h6 className="card-title text-center">
+                                <a href={user.html_url} className="card-link">{user.name}</a>
+                            </h6>
+                        </div>
+                        <ul className="list-group list-group-flush">
+                            <li className="list-group-item">Company: {user.company}</li>
+                            <li className="list-group-item">Location: {user.location}</li>
+                            <li className="list-group-item">Email: {user.email}</li>
+                            <li className="list-group-item">Followers: {user.followers}</li>
+                            <li className="list-group-item">Following: {user.following}</li>
+                            <li className="list-group-item">Public Repos: {user.public_repos}</li>
+                            <li className="list-group-item">Public Gists: {user.public_gists}</li>
+                            <li className="list-group-item">Member Since: {user.created_at}</li>
+                        </ul>
                     </div>
-                    <ul className="list-group list-group-flush">
-                        <li className="list-group-item">An item</li>
-                        <li className="list-group-item">A second item</li>
-                        <li className="list-group-item">A third item</li>
-                    </ul>
-                    <div className="card-body">
-                        <a href="#" className="card-link">Card link</a>
-                        <a href="#" className="card-link">Another link</a>
-                    </div>
-                </div>
+                </Link>
             </div>
         </>
     )
